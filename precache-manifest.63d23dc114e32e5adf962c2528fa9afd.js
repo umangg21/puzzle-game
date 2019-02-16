1cba57eaad0a0895d5fa3e8fd5fa6e33ef8d8962
umangg21 <umangg21@gmail.com>
2019-02-16 13:12:09 +0530

    Updates

---
+++ precache-manifest.63d23dc114e32e5adf962c2528fa9afd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d1a2729193ecb34fbfb",
+    "url": "/puzzle-game/static/js/runtime~main.4d1a2729.js"
+  },
+  {
+    "revision": "fafab27f3d905e38e784",
+    "url": "/puzzle-game/static/js/main.fafab27f.chunk.js"
+  },
+  {
+    "revision": "a8c80c76f10e5715b8d4",
+    "url": "/puzzle-game/static/js/2.a8c80c76.chunk.js"
+  },
+  {
+    "revision": "fafab27f3d905e38e784",
+    "url": "/puzzle-game/static/css/main.64164ccf.chunk.css"
+  },
+  {
+    "revision": "73056b0e4019c6e277086e79bdfa755e",
+    "url": "/puzzle-game/index.html"
+  }
+];
